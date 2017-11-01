20a24e93c89db3aeb8065f5db9607000b4ead676
Christoph Olszowka <christoph@olszowka.de>
2017-11-01 21:10:46 +0100

    Drop unneccessary eager load

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 class WelcomeController < ApplicationController
   def home
-    @groups = CategoryGroup.order(name: :asc).includes(categories: :projects)
+    @groups = CategoryGroup.order(name: :asc).includes(:categories)
     render action: :home
   end
 end
