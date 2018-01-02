6904a7a1baaa727d18f22a61aefc6b184c33f504
Christoph Olszowka <christoph@olszowka.de>
2018-01-02 22:12:48 +0100

    Add rubygems index sync job from earlier prototype

---
+++ spec/jobs/rubygems_sync_job_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe RubygemsSyncJob, type: :job do
+  let(:job) { described_class.new }
+
+  describe "#remote_gems" do
+    it "acquires the latest rubygems specs and returns all gem names" do
+      source = instance_double(::Gem::Source)
+
+      allow(::Gem::Source).to receive(:new)
+        .with("https://rubygems.org")
+        .and_return(source)
+
+      specs = [OpenStruct.new(name: "foo"), OpenStruct.new(name: "bar")]
+      allow(source).to receive(:load_specs)
+        .with(:latest).and_return(specs)
+
+      expect(job.remote_gems).to be == %w[foo bar]
+    end
+  end
+
+  describe "#local_gems" do
+    it "is a collection of names of local gems" do
+      allow(Rubygem).to receive(:pluck).with(:name).and_return(%w[foo bar])
+      expect(job.local_gems).to be == %w[foo bar]
+    end
+  end
+
+  describe "#perform" do
+    let(:local_gems)  { %w[rspec what simplecov] }
+    let(:remote_gems) { %w[rspec rails simplecov] }
+
+    before do
+      allow(job).to receive(:local_gems).and_return(local_gems)
+      allow(job).to receive(:remote_gems).and_return(remote_gems)
+    end
+
+    it "triggers update jobs for all locally missing gems" do
+      allow(RubygemUpdateJob).to receive(:perform_async).with("what")
+      expect(RubygemUpdateJob).to receive(:perform_async).with("rails")
+      job.perform
+    end
+
+    it "triggers update jobs for all remotely missing gems" do
+      allow(RubygemUpdateJob).to receive(:perform_async).with("rails")
+      expect(RubygemUpdateJob).to receive(:perform_async).with("what")
+      job.perform
+    end
+  end
+end
