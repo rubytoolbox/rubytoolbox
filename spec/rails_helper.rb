f8f98c9f42a3513f3bcaf263c42b246716662806
Christoph Olszowka <christoph@olszowka.de>
2018-01-03 21:45:09 +0100

    Enable sidekiq testing mode

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
+require "sidekiq/testing"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # It's early stages, and SimpleCov is not happy about those not being loaded
