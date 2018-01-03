c02b09660c87c9fb34cb5b3f4791bb1a9e2ad9b1
Christoph Olszowka <christoph@olszowka.de>
2018-01-03 23:35:26 +0100

    Move metric display to a partial/helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def metric(label, value, icon:)
+    render partial: "projects/metric", locals: { label: label, value: value, icon: icon }
+  end
 end
