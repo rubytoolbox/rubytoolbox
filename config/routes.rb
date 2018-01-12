d52685b5cc8ddb9522c2f6a27f5ef66c946b73ea
Christoph Olszowka <christoph@olszowka.de>
2018-01-12 22:13:58 +0100

    Move patterns constants to separate file since rails route reload spits a lot of warnings

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
+require "patterns"
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Ripped from https://github.com/rubygems/rubygems.org/blob/master/lib/patterns.rb and enhanced
-  # to also allow slashes as per github
-  SPECIAL_CHARACTERS    = ".-_/"
-  ALLOWED_CHARACTERS    = "[A-Za-z0-9#{Regexp.escape(SPECIAL_CHARACTERS)}]+"
-  ROUTE_PATTERN         = /#{ALLOWED_CHARACTERS}/
-
   resources :categories, only: %i[show]
-  resources :projects, only: %i[show], constraints: { id: ROUTE_PATTERN }
+  resources :projects, only: %i[show], constraints: { id: Patterns::ROUTE_PATTERN }
 
   root "welcome#home"
 end
