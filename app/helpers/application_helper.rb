bdad9b334b2185f97935d04a15090e48b6bf7463
Christoph Olszowka <christoph@olszowka.de>
2018-01-14 22:22:48 +0100

    Add og and twitter card meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
   def metric(label, value, icon:)
     render partial: "projects/metric", locals: { label: label, value: value, icon: icon }
   end
+
+  def title
+    ["The Ruby Toolbox", content_for(:title).presence || "Know your options!"].join(" - ")
+  end
+
+  def description
+    content_for(:description).presence || "Explore and compare open source Ruby libraries"
+  end
 end
