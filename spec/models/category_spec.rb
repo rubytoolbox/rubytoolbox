e85d9846863873df138e578ce2ea87232f7239a8
Christoph Olszowka <christoph@olszowka.de>
2018-01-30 23:21:58 +0100

    Invite catalog contribution by adding a edit button to each category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,14 +3,34 @@
 require "rails_helper"
 
 RSpec.describe Category, type: :model do
+  let(:group) do
+    CategoryGroup.create! permalink: "unimportant", name: "unimportant"
+  end
+  let(:category) do
+    Category.create! permalink: "mocking", name: "Mocking Frameworks", category_group: group
+  end
+
   describe ".search" do
     it "can find a matching category" do
-      group = CategoryGroup.create! permalink: "unimportant", name: "unimportant"
-      expected = Category.create! permalink: "mocking", name: "Mocking Frameworks", category_group: group
+      category
       Category.create! permalink: "foo", name: "Foo", category_group: group
       Category.create! permalink: "bar", name: "Bar", category_group: group
 
-      expect(Category.search("mock")).to be == [expected]
+      expect(Category.search("mock")).to be == [category]
+    end
+  end
+
+  describe "#catalog_show_url" do
+    it "is the url where the category definition can be seen on github" do
+      expected = "https://github.com/rubytoolbox/catalog/tree/master/catalog/unimportant/mocking.yml"
+      expect(category.catalog_show_url).to be == expected
+    end
+  end
+
+  describe "#catalog_edit_url" do
+    it "is the url where the category definition can be edited on github" do
+      expected = "https://github.com/rubytoolbox/catalog/edit/master/catalog/unimportant/mocking.yml"
+      expect(category.catalog_edit_url).to be == expected
     end
   end
 end
