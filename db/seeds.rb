a7375c514578e16e0b4434ea0f18c369e8b0643d
Christoph Olszowka <christoph@olszowka.de>
2018-09-30 21:49:56 +0200

    Add a note to db:seed script about bin/pull_database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# rubocop:disable Rails/Output
+puts "Note: You can use `bin/pull_database` to fetch and import a production database dump from https://data.ruby-toolbox.com"
+# rubocop:enable Rails/Output
