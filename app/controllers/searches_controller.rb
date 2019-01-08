9ba5a9e0d17744df5a8e1a572af5d20899a9ca48
Christoph Olszowka <christoph@olszowka.de>
2019-01-08 14:30:44 +0100

    Enable custom sorting in searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,13 @@
 class SearchesController < ApplicationController
   def show
     @query = params[:q].presence
-    @search = Search.new(@query) if @query
+    @search = Search.new(@query, order: current_order) if @query
   end
+
+  private
+
+  def current_order
+    @current_order ||= Project::Order.new order: params[:order], directions: Project::Order::SEARCH_DIRECTIONS
+  end
+  helper_method :current_order
 end
