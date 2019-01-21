6a3056972eaec8dcce66ee25a74d0c7d1aceb209
Christoph Olszowka <christoph@olszowka.de>
2019-01-21 13:38:20 +0100

    Add navigation docs link and entry page

--- spec/features/docs_spec.rb
+++ spec/features/docs_spec.rb
@@ -4,8 +4,18 @@ require "rails_helper"
 
 RSpec.describe "Documentation Display", type: :feature, js: true do
   it "can display all documentation pages" do
+    visit "/"
+
+    within "header.main .navbar" do
+      click_on "Docs"
+    end
+    expect(page).to have_text "Welcome to the Ruby Toolbox documentation"
+
     Docs.new.sections.map(&:last).flatten.each do |doc|
-      visit page_path(doc)
+      within "aside.menu" do
+        click_on doc.title
+      end
+
       within ".hero" do
         expect(page).to have_text(doc.title)
       end
