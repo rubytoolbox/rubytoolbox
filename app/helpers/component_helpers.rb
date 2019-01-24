074ae3cb937cc07fe88c31ae772f8dce30712802
Christoph Olszowka <christoph@olszowka.de>
2019-01-24 15:10:04 +0100

    Prototyping project compare table component

--- app/helpers/component_helpers.rb
+++ app/helpers/component_helpers.rb
@@ -59,6 +59,10 @@ module ComponentHelpers
     render "components/project_order_dropdown", order: order
   end
 
+  def project_comparison(projects)
+    render "components/project_comparison", projects: projects
+  end
+
   def section_heading(title, description: nil, &block)
     render "components/section_heading", title: title, description: description, &block
   end
