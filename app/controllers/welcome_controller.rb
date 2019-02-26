19ad636090b072d3552de49b5a41bbebe5ace4a6
Christoph Olszowka <christoph@olszowka.de>
2019-02-26 09:49:06 +0100

    Add recently added categories landing page section

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@
 class WelcomeController < ApplicationController
   def home
     @featured_categories = Category.featured
+    @new_categories = Category.recently_added
     @stats = Stats.new
     @recent_posts = BlogController::BLOG.recent_posts.presence
   end
