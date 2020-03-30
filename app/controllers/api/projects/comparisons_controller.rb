527896ab6731210b2518542cb060ce7f61914a5f
Christoph Olszowka <christoph@olszowka.de>
2020-03-30 22:11:46 +0200

    Return an error when too many projects are requested instead of silently ignoring some

--- app/controllers/api/projects/comparisons_controller.rb
+++ app/controllers/api/projects/comparisons_controller.rb
@@ -1,17 +1,31 @@
 # frozen_string_literal: true
 
 class Api::Projects::ComparisonsController < ActionController::API
+  LIMIT = 100
+  before_action :verify_limit, only: %i[show]
+
   def show
     projects = Project.where(permalink: requested_project_permalinks)
                       .for_display(forks: true)
                       .includes_associations
                       .order(permalink: :asc)
-                      .limit(100)
+                      .limit(LIMIT)
 
     render json: ProjectBlueprint.render(projects, root: :projects, root_url: request_root_url)
   end
 
   private
+
+  def verify_limit
+    return if requested_project_permalinks.count <= LIMIT
+
+    response_body = {
+      error_code: "too_many_projects_requested",
+      message:    "Please request no more than #{LIMIT} projects per API call",
+    }
+
+    render json: response_body, status: 400
+  end
 
   def requested_project_permalinks
     (params[:id].presence || "").split(",")
