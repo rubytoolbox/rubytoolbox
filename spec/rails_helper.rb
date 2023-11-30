6ca9d5d494e680f3dabdf618ee772c994450d075
Christoph Olszowka <christoph@olszowka.de>
2023-11-30 15:20:23 +0100

    Fix deprecation by passing fixture path as array

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,7 +74,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = Rails.root.join "spec", "fixtures"
+  config.fixture_path = [Rails.root.join("spec", "fixtures")]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
