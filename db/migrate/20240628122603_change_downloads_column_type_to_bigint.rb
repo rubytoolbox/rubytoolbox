73b9cf77f12a3f5328fef6eadf1eb0c6034e5849
Christoph Olszowka <christoph@olszowka.de>
2024-06-28 15:16:37 +0200

    Draft out safer take on the column migration

--- db/migrate/20240628122603_change_downloads_column_type_to_bigint.rb
+++ db/migrate/20240628122603_change_downloads_column_type_to_bigint.rb
@@ -2,8 +2,24 @@
 
 class ChangeDownloadsColumnTypeToBigint < ActiveRecord::Migration[7.1]
   def up
-    change_column :rubygems, :downloads, :bigint, null: false
-    change_column :rubygem_download_stats, :total_downloads, :bigint, null: false
+    add_column :rubygems, :bigint_downloads, :bigint
+    add_column :rubygem_download_stats, :bigint_total_downloads, :bigint
+
+    # rubocop:disable Rails/SkipsModelValidations
+    Rubygem.update_all "bigint_downloads = downloads"
+    Rubygem::DownloadStat.update_all "bigint_total_downloads = total_downloads"
+    # rubocop:enable Rails/SkipsModelValidations
+
+    remove_column :rubygems, :downloads
+    remove_column :rubygem_download_stats, :total_downloads
+
+    rename_column :rubygems, :bigint_downloads, :downloads
+    rename_column :rubygem_download_stats, :bigint_total_downloads, :total_downloads
+
+    change_column_null :rubygems, :downloads, false
+    change_column_null :rubygem_download_stats, :total_downloads, false
+
+    add_index :rubygem_download_stats, :total_downloads, order: "DESC NULLS LAST", algorithm: :concurrently
   end
 
   def down
