1bce170298b67aefaaaad7394d6e2ee19ed3dec2
Christoph Olszowka <christoph@olszowka.de>
2024-06-28 11:13:31 +0200

    Introduce a wrapper struct for accessing the advisory info

--- app/models/rubygem/advisory.rb
+++ app/models/rubygem/advisory.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
 
 class Rubygem::Advisory < ApplicationRecord
+  #
+  # Just a simple wrapper on the base data we get from the bundler-audit
+  # gem to make it accessible via a real ruby object
+  #
+  class Info < ApplicationStruct
+    attribute :id, Types::Strict::String
+    attribute :url, Types::Strict::String.optional
+    attribute :date, Types::Params::Date
+
+    attribute :cve, Types::Strict::String.optional
+    attribute :ghsa, Types::Strict::String.optional
+    # We make this coercible because those are integers, and hence
+    # during conversion they end up as ints on the database
+    attribute :osvdb, Types::Coercible::String.optional
+
+    attribute :cvss_v2, Types::Coercible::Float.optional
+    attribute :cvss_v3, Types::Coercible::Float.optional
+    attribute :criticality, Types::Strict::String.optional
+
+    attribute :title, Types::Strict::String.optional
+    attribute :description, Types::Strict::String.optional
+  end
+
   belongs_to :rubygem,
              primary_key: :name,
              foreign_key: :rubygem_name,
@@ -8,4 +31,8 @@ class Rubygem::Advisory < ApplicationRecord
 
   validates :date, presence: true
   validates :rubygem_name, presence: true
+
+  def info
+    @info ||= Info.new advisory_data
+  end
 end
--- spec/models/rubygem/advisory_spec.rb
+++ spec/models/rubygem/advisory_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe Rubygem::Advisory do
 
   it { expect(model).to validate_presence_of(:date) }
   it { expect(model).to validate_presence_of(:rubygem_name) }
+
+  describe "#info" do
+    subject(:info) { rubygem_advisories(:nokogiri1).info }
+
+    it { is_expected.to be_a(ApplicationStruct) }
+
+    it do # rubocop:disable RSpec/ExampleLength
+      expect(info).to have_attributes(
+        id:          "CVE-2019-18197",
+        cve:         "2019-18197",
+        url:         "https://github.com/sparklemotion/nokogiri/issues/1943",
+        date:        Date.new(2022, 5, 24),
+        ghsa:        "242x-7cm6-4w8j",
+        osvdb:       nil,
+        title:       "Nokogiri affected by libxslt Use of Uninitialized Resource/ Use After Free vulnerability",
+        cvss_v2:     5.1,
+        cvss_v3:     7.5,
+        criticality: "high",
+        description: /\AIn xsltCopyText/
+      )
+    end
+  end
 end
