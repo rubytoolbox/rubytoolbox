4d3d3a6812da13202c7e0a5bdfd4fa2cd012bd23
Christoph Olszowka <christoph@olszowka.de>
2024-06-28 11:30:53 +0200

    Add delegates from advisory to it's info

--- app/models/rubygem/advisory.rb
+++ app/models/rubygem/advisory.rb
@@ -22,6 +22,8 @@ class Rubygem::Advisory < ApplicationRecord
 
     attribute :title, Types::Strict::String.optional
     attribute :description, Types::Strict::String.optional
+
+    alias identifier id
   end
 
   belongs_to :rubygem,
@@ -32,7 +34,12 @@ class Rubygem::Advisory < ApplicationRecord
   validates :date, presence: true
   validates :rubygem_name, presence: true
 
+  delegate(*Rubygem::Advisory::Info.instance_methods(false).excluding(:id, :date), to: :info)
+
   def info
+    # If the underlying data changes, the info struct would get stale,
+    # but that's fine because normally this data is updated out-of-band in the
+    # advisory sync job, so no need for adding complex memo busting logic
     @info ||= Info.new advisory_data
   end
 end
--- spec/models/rubygem/advisory_spec.rb
+++ spec/models/rubygem/advisory_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe Rubygem::Advisory do
   fixtures :all
 
-  subject(:model) { described_class.new }
+  subject(:model) { rubygem_advisories(:nokogiri1) }
 
   it {
     expect(model).to belong_to(:rubygem)
@@ -17,14 +17,30 @@ RSpec.describe Rubygem::Advisory do
   it { expect(model).to validate_presence_of(:date) }
   it { expect(model).to validate_presence_of(:rubygem_name) }
 
+  %i[
+    criticality
+    cve
+    cvss_v2
+    cvss_v3
+    ghsa
+    identifier
+    osvdb
+    title
+    url
+    description
+  ].each do |property|
+    it { is_expected.to delegate_method(property).to(:info) }
+  end
+
   describe "#info" do
-    subject(:info) { rubygem_advisories(:nokogiri1).info }
+    subject(:info) { model.info }
 
     it { is_expected.to be_a(ApplicationStruct) }
 
     it do # rubocop:disable RSpec/ExampleLength
       expect(info).to have_attributes(
         id:          "CVE-2019-18197",
+        identifier:  "CVE-2019-18197",
         cve:         "2019-18197",
         url:         "https://github.com/sparklemotion/nokogiri/issues/1943",
         date:        Date.new(2022, 5, 24),
